0a51a0b8917f2893581da4a093e88fc7128562bf
ismail bilal <73436909+ismailbilal@users.noreply.github.com>
2020-10-27 17:06:27 +0100

    ax+by=c and fonctions
    just uploading the first code

---
+++ ax+by=c.py
@@ -0,0 +1,17 @@
+from fonctions import fGCD,initial_solution,write_y,write_x,write_coef
+print('to solve a Linear Diophantine Equation ,ax+by=c')
+a=int(input('enter a : '))
+b=int(input('enter b : '))
+c= int(input('enter c : '))
+gcd=fGCD(a, b)
+if c%gcd != 0 :
+    print("there are no solution.")
+else:
+    (x,y)=initial_solution(a,b,c)
+    bi=int(b/gcd)
+    ai=int(a/gcd)
+    x=write_x(x)
+    y=write_y(y)
+    bi=write_coef(bi)
+    ai=write_coef(ai)
+    print(f'the solutions of this Diophantine equation is (x,y)=[({x}{bi}k,{y}{ai}k)/ for k integer]')
---
+++ fonctions.py
@@ -0,0 +1,58 @@
+#this fonction is for calculate the greatest common divisor
+def fGCD(a,b) :
+    if abs(a) < abs(b) :
+        q=b
+        r=a
+    else:
+        q=a
+        r=b
+    while q%r != 0 :
+        l=q%r
+        q=r
+        r=l
+    return r 
+
+
+#this fonction is for calculate an initial solution
+def initial_solution(a,b,c) :
+    #~~~~find an initial integer solution
+    y=1
+    gcd=fGCD(a,b)
+    n=abs(gcd-(b*y))
+    ci=int(c/gcd)
+    while n%a != 0 :
+        if y>0 :
+            y=-y
+        else:
+            y=-y+1
+        n=abs(gcd-(b*y))
+    n=gcd-(b*y)
+    x=n/a
+    #~~~~~~
+
+
+    y=int(y*ci)
+    x=int(x*ci)
+    return (x,y)
+
+
+#to make solution looks better
+def write_x(x) :
+    wr_var = {True : f'{x}+', False : ""}
+    return wr_var[
+        x != 0
+    ]
+
+
+def write_y(x) :
+    wr_var = {True : f'{x}-', False : ""}
+    return wr_var[
+        x != 0
+    ]
+
+
+def write_coef(x) :
+    wr_coef = {True : f'{x}', False : ""}
+    return wr_coef[
+        x != 1
+    ]
